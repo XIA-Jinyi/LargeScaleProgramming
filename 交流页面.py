57ccdb92c831bf52b100484c1a4c9a44ba0334c1
liam1221 <101781384+liam1221@users.noreply.github.com>
2023-06-26 16:17:08 +0800

    test

---
+++ 交流页面.py
@@ -0,0 +1,23 @@
+'''
+页面功能通信录管理功能:实现对通信录好友的添加、删除
+即时通讯功能：实现与通信录好友间的信息发送/接收(有个窗口实现基本的信息显示)
+信息隐藏功能：实现以图片为载体的透明嵌入提取功能(发送图片和图片信息提取按键)
+用户在线列表：负责向用户发送其好友的在线列表（包括好友用户名\在线状态）
+用户聊天列表
+'''
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+if __name__ == '__main__':
+
+    # app = QApplication(sys.argv)
+    #
+    # w = QWidget()
+    # w.resize(250, 150)
+    # w.move(300, 300)
+    # w.setWindowTitle('Simple')
+    # w.show()
+    #
+    # sys.exit(app.exec_())
+    print("h")
